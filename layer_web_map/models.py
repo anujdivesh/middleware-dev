1afba7d5c5ce3e6615a82afc9d43356accac6e8e
anujdivesh <divesha@spc.int>
2024-09-10 15:33:48 +1200

    final

--- layer_web_map/models.py
+++ layer_web_map/models.py
@@ -5,15 +5,19 @@ class LayerWebMap(models.Model):
     enabled = models.BooleanField()
     zoomToLayer = models.BooleanField()
     url = models.CharField(max_length=255)
+    on_click_url = models.CharField(max_length=255,null=True,blank=True)
     layer_title = models.CharField(max_length=255)
     layer_type = models.CharField(max_length=255)
     is_timeseries = models.BooleanField()
+    datetime_format = models.CharField(max_length=255)
     image_format = models.CharField(max_length=255)
     layer_name = models.CharField(max_length=255)
     transparent = models.BooleanField()
+    update_transparency = models.BooleanField()
     style = models.CharField(max_length=255)
     colormin = models.IntegerField()
     colormax = models.IntegerField()
+    update_color = models.BooleanField()
     abovemaxcolor = models.CharField(max_length=255)
     belowmincolor = models.CharField(max_length=255)
     numcolorbands = models.IntegerField(default=250)
@@ -22,6 +26,7 @@ class LayerWebMap(models.Model):
     period = models.CharField(max_length=255)
     opacity = models.FloatField(null=True,blank=True)
     legend_url = models.CharField(max_length=255)
+    update_legend = models.BooleanField()
 
     def __str__(self):
         return f"{self.layer_title}"
--- restore_db.sh
+++ restore_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker cp backup_20_August.sql middleware_db:/var/lib/postgresql/data/6Septback.sql
-docker exec -it middleware_db psql -U postgres -d "ocean-middleware" -f /var/lib/postgresql/data/6Septback.sql
+docker cp 10_sept_backup.sql middleware_db:/var/lib/postgresql/data/10_sept_backup.sql
+docker exec -it middleware_db psql -U postgres -d "ocean-middleware" -f /var/lib/postgresql/data/10_sept_backup.sql
