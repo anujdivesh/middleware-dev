78c14aafa7c501073f8af021dd16ae2eccd94ac7
Anuj Divesh <divesha@spc.int>
2025-08-12 15:51:15 +1200

    added db restore

--- restore_db.sh
+++ restore_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker cp 8Julywidget.sql middleware_db:/var/lib/postgresql/data/8Julywidget.sql
-docker exec -it middleware_db psql -U postgres -d "ocean-middleware" -f /var/lib/postgresql/data/8Julywidget.sql
+docker cp 12Aug_1.sql middleware_db:/var/lib/postgresql/data/12Aug_1.sql
+docker exec -it middleware_db psql -U postgres -d "ocean-middleware" -f /var/lib/postgresql/data/12Aug_1.sql
