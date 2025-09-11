644c8f8a0cbb74c308689ac277e8c566e0f2aa2b
Anuj Divesh <divesha@spc.int>
2025-09-12 10:25:56 +1200

    added enabling for themes

--- restore_db.sh
+++ restore_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker cp 26_Aug_2.sql middleware_db:/var/lib/postgresql/data/26_Aug_2.sql
-docker exec -it middleware_db psql -U postgres -d "ocean-middleware" -f /var/lib/postgresql/data/26_Aug_2.sql
+docker cp 12_Sept2.sql middleware_db:/var/lib/postgresql/data/12_Sept2.sql
+docker exec -it middleware_db psql -U postgres -d "ocean-middleware" -f /var/lib/postgresql/data/12_Sept2.sql
--- theme/models.py
+++ theme/models.py
@@ -2,6 +2,7 @@ from django.db import models
 # Create your models here.
 class Theme(models.Model):
     name = models.CharField(max_length=255)
+    enabled = models.BooleanField()
 
     def __str__(self):
         return f"{self.name}"
